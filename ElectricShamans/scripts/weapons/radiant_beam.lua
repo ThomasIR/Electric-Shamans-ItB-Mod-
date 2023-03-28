b5155e2296f7507e76e8a6edf2100c2a083bc77d
ThomasIR <32403845+ThomasIR@users.noreply.github.com>
2023-03-27 18:08:14 -0700

    Update radiant_beam.lua
    Changed impact animations

--- ElectricShamans/scripts/weapons/radiant_beam.lua
+++ ElectricShamans/scripts/weapons/radiant_beam.lua
@@ -50,17 +50,25 @@ function Radiant_Beam:GetSkillEffect(p1,p2)
 	for i = 1, distance do
 		local curr = p1 + DIR_VECTORS[direction]*i
 		local push = (i == distance) and direction*self.Push or DIR_NONE
+
 		if Board:IsPawnSpace(curr) then
 			local damage = self.Damage
 			if Board:IsPawnTeam(curr, TEAM_PLAYER) then
 				damage = -damage
 			end
-			local spaceDamage = SpaceDamage(p1 + DIR_VECTORS[direction]*i,damage, push)
-			spaceDamage.sAnimation = "ExploAir1"
+
+			local spaceDamage = SpaceDamage(curr, damage, push)
+			spaceDamage.sAnimation = "Lightning_Hit"
 			if i == distance then 	
 				spaceDamage.sAnimation = "flamethrower"..distance.."_"..direction 
 			end
 			ret:AddDamage(spaceDamage)
+
+			if spaceDamage.iDamage > 0 then
+				local dummy_damage = SpaceDamage(curr, 0)
+				dummy_damage.sAnimation = "ExploAir1"
+				ret:AddDamage(dummy_damage)
+			end
 		end
 	end
 
