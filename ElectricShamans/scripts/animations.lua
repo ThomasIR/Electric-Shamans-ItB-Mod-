629e390e7b3322db8e4a02b8a124625566349f58
ThomasIR <32403845+ThomasIR@users.noreply.github.com>
2024-01-21 01:46:35 -0800

    Add files via upload
    Added necessary code for new Draining Scythe animation

---
+++ ElectricShamans/scripts/animations.lua
@@ -0,0 +1,25 @@
+ANIMS.para_scythe_attack_0 = Animation:new{
+	Image = "effects/para_scythe_attack_U.png",
+	NumFrames = 6,
+	Time = 0.07,
+	PosX = -36,
+	PosY = -5
+}
+
+ANIMS.para_scythe_attack_1 = ANIMS.para_scythe_attack_0:new{
+	Image = "effects/para_scythe_attack_R.png",
+	PosX = -36,
+	PosY = -12
+}
+
+ANIMS.para_scythe_attack_2 = ANIMS.para_scythe_attack_0:new{
+	Image = "effects/para_scythe_attack_D.png",
+	PosX = -21,
+	PosY = -12
+}
+
+ANIMS.para_scythe_attack_3 = ANIMS.para_scythe_attack_0:new{
+	Image = "effects/para_scythe_attack_L.png",
+	PosX = -21,
+	PosY = -5
+}
